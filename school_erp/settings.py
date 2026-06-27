2d18e448f97e6a9d34983c6fbf91d0bbb5020fa4
Dafirmcoder <dafirmcyber@gmail.com>
2026-06-27 15:56:28 +0300

    Fix CSRF verification failed by adding .up.railway.app to CSRF_TRUSTED_ORIGINS

--- school_erp/settings.py
+++ school_erp/settings.py
@@ -136,3 +136,6 @@ LOGIN_REDIRECT_URL = 'dashboard'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['https://*.up.railway.app'])
+

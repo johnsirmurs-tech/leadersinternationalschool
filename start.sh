93e7ebb31354d4982cd0ed81a96ad467dfe1f1b4
Dafirmcoder <dafirmcyber@gmail.com>
2026-07-07 07:51:22 +0300

    deploy: remove setup_initial_data seeding from start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py migrate && python manage.py setup_initial_data && gunicorn school_erp.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+python manage.py migrate && gunicorn school_erp.wsgi:application --bind 0.0.0.0:${PORT:-8000}
